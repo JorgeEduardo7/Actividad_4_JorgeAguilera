85f8eb1719c3f8502cc6afd0c14495d6ab2d44df
unknown <jorgeeduardoaguilerapantoja@gmail.com>
2024-04-02 10:33:41 -0400

    Actividad 4

---
+++ Actividad_4_JORGE/Actividad.VISTA/PedidoVista/PedidoInterfaz.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace Actividad.VISTA.PedidoVista
+{
+    partial class PedidoInterfaz
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PedidoInterfaz));
+            button3 = new Button();
+            button2 = new Button();
+            button1 = new Button();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            dataGridView1 = new DataGridView();
+            dateTimePicker1 = new DateTimePicker();
+            button4 = new Button();
+            colorDialog1 = new ColorDialog();
+            button5 = new Button();
+            panel1 = new Panel();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            SuspendLayout();
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.FromArgb(246, 220, 52);
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Font = new Font("Segoe UI Symbol", 10F, FontStyle.Bold, GraphicsUnit.Point);
+            button3.Location = new Point(405, 432);
+            button3.Name = "button3";
+            button3.Size = new Size(116, 48);
+            button3.TabIndex = 27;
+            button3.Text = "ELIMINAR";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.FromArgb(246, 220, 52);
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Segoe UI Symbol", 10F, FontStyle.Bold, GraphicsUnit.Point);
+            button2.Location = new Point(240, 432);
+            button2.Name = "button2";
+            button2.Size = new Size(116, 48);
+            button2.TabIndex = 26;
+            button2.Text = "EDITAR";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(246, 220, 52);
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Segoe UI Symbol", 10F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.ForeColor = SystemColors.ControlText;
+            button1.Location = new Point(572, 432);
+            button1.Name = "button1";
+            button1.Size = new Size(116, 48);
+            button1.TabIndex = 25;
+            button1.Text = "AGREGAR";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.FromArgb(157, 13, 20);
+            label3.Font = new Font("Gill Sans MT", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.ForeColor = Color.FromArgb(246, 220, 52);
+            label3.Location = new Point(651, 65);
+            label3.Name = "label3";
+            label3.Size = new Size(51, 18);
+            label3.TabIndex = 22;
+            label3.Text = "FECHA";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.FromArgb(157, 13, 20);
+            label2.Font = new Font("Gill Sans MT", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(408, 73);
+            label2.Name = "label2";
+            label2.Size = new Size(53, 18);
+            label2.TabIndex = 21;
+            label2.Text = "TOTAL";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.FromArgb(157, 13, 20);
+            label1.Font = new Font("Gill Sans MT", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(38, 65);
+            label1.Name = "label1";
+            label1.Size = new Size(77, 18);
+            label1.TabIndex = 20;
+            label1.Text = "IDCLIENTE";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(464, 42);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(141, 23);
+            textBox2.TabIndex = 16;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(118, 42);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(238, 23);
+            textBox1.TabIndex = 15;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToDeleteRows = false;
+            dataGridView1.BackgroundColor = SystemColors.ButtonFace;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(178, 115);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.ReadOnly = true;
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView1.Size = new Size(717, 311);
+            dataGridView1.TabIndex = 14;
+            dataGridView1.Click += dataGridView1_Click;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(705, 39);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(200, 23);
+            dateTimePicker1.TabIndex = 28;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.FromArgb(253, 196, 3);
+            button4.FlatStyle = FlatStyle.Flat;
+            button4.Font = new Font("Sylfaen", 10F, FontStyle.Bold, GraphicsUnit.Point);
+            button4.Location = new Point(171, 76);
+            button4.Name = "button4";
+            button4.Size = new Size(136, 32);
+            button4.TabIndex = 29;
+            button4.Text = "SELECCIONAR";
+            button4.UseVisualStyleBackColor = false;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.BackColor = Color.FromArgb(246, 220, 52);
+            button5.FlatStyle = FlatStyle.Flat;
+            button5.Font = new Font("Segoe UI Symbol", 10F, FontStyle.Bold, GraphicsUnit.Point);
+            button5.ForeColor = SystemColors.ControlText;
+            button5.Location = new Point(718, 432);
+            button5.Name = "button5";
+            button5.Size = new Size(116, 48);
+            button5.TabIndex = 30;
+            button5.Text = "SALIR";
+            button5.UseVisualStyleBackColor = false;
+            button5.Click += button5_Click;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(157, 13, 20);
+            panel1.Controls.Add(pictureBox2);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(button4);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(label3);
+            panel1.Location = new Point(-3, -5);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1015, 114);
+            panel1.TabIndex = 31;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(44, 9);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(64, 59);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 30;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = Properties.Resources.dinero;
+            pictureBox2.Location = new Point(397, 11);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(64, 59);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 31;
+            pictureBox2.TabStop = false;
+            // 
+            // PedidoInterfaz
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(234, 160, 54);
+            ClientSize = new Size(991, 564);
+            Controls.Add(button5);
+            Controls.Add(textBox2);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(dataGridView1);
+            Controls.Add(panel1);
+            Name = "PedidoInterfaz";
+            Text = "PedidoInterfaz";
+            Load += PedidoInterfaz_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button3;
+        private Button button2;
+        private Button button1;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private DataGridView dataGridView1;
+        private DateTimePicker dateTimePicker1;
+        private Button button4;
+        private ColorDialog colorDialog1;
+        private Button button5;
+        private Panel panel1;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+    }
+}
